aded6415a0c6e5b9bb8b76b2fa7c58f43f67e61e
steven <stevenlahore@gmail.com>
2024-12-23 23:53:35 +0800

    the second time commit file for github  for makefile_test

---
+++ makefile_test/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "mathhead.h"
+
+
+int main()
+{
+#ifdef DEBUG
+	printf("this is a DEBUG mode the DEBUG macro is:%d\n",DEBUG);
+#endif
+	int x,y;
+	x=10;
+	y=5;
+	int a=add(x,y);
+	int s=sub(x,y);
+	int m=mul(x,y);
+	int am=div(x,y);
+	printf("hello the resault of matc x add y is :%d\n",a);
+	printf("hello the resault of matc x sub y is :%d\n",s);
+	printf("hello the resault of matc x mul y is :%d\n",m);
+	printf("hello the resault of matc x div y is :%d\n",am);
+	return (0);
+}
---
+++ makefile_test/mathhead.h
@@ -0,0 +1,5 @@
+//this is the function of math
+int add (int a,int b);
+int sub (int a,int b);
+int mul (int a,int b);
+int div (int a,int b);
